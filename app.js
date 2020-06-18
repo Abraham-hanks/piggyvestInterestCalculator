13ce5656e79f8b0d0056682d0336ec2d9f1276f5
Abraham <ekekhorabraham@gmail.com>
2020-06-18 11:43:59 +0100

    added something

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
 const authRoutes = require('./routes/authRoute');
 let port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
